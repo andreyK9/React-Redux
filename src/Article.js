f3d9342437fd91010c53f8dbcb62cef299856841
andreyK9 <18041988@mail.ru>
2018-08-28 11:45:35 +0300

    add Article

---
+++ src/Article.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+export default class Article extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isOpen: false
+    }
+  }
+  
+  render() {
+    const { article } = this.props;
+    const {isOpen} = this.state;
+    
+    return (
+      <div>
+        <h3>{article.title}</h3>
+        <button onClick = {this.toggleOpen}>
+        {isOpen ? 'close' : 'open'}
+        </button>
+        {this.getBody()}
+      </div>
+    )
+  }
+
+  toggleOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
+  getBody() {
+    const {article} = this.props;
+    return this.state.isOpen ? 
+    <section>{article.text}</section> 
+    : 
+    null;
+  }
+};
+
